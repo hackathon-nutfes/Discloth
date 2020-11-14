bf9d30c8b3d7d03a74ba317d363f1d337674dc68
Kugue99A <hisabisa99yohey@gmail.com>
2020-11-14 23:38:42 +0900

    [mod]welcome

--- view/vue-project/src/views/Welcome.vue
+++ view/vue-project/src/views/Welcome.vue
@@ -1,12 +1,21 @@
 <template> 
   <v-app>
     <v-content class="bg">
+      <Login />
+      <Createid />
     </v-content>
   </v-app>
 </template> 
 
 <script> 
+  import Createid from "../components/Createid.vue"
+  import Login from "../components/Login.vue"
   export default { 
+    name: "Welcome",
+    components: {
+      Login,
+      Createid
+    }
   } 
 </script> 
 <style>
