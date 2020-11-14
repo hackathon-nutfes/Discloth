7b3316b962121e0d6d8a1c20aadba80fc6049375
TomoeYasuda <tomoe.pierrot3@gmail.com>
2020-11-15 00:24:07 +0900

    connect

--- api/app/models/item.rb
+++ api/app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
+    belongs_to :color
+    belongs_to :design
+    belongs_to :gender
+    belongs_to :kind
+    belongs_to :price
+    belongs_to :season
+    belongs_to :size
 end
--- api/app/models/price.rb
+++ api/app/models/price.rb
@@ -1,2 +1,3 @@
 class Price < ApplicationRecord
+    has_many :items
 end
