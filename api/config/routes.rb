b2beed85f81f51d292a028e91dbffc576266ce03
oba18 <m.oba.0426@gmail.com>
2020-11-14 23:37:42 +0900

    #1

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :items
+  # resources :clothes
+  get "items" => "items#index"
+  get "items/:id" => "items#show"
+  post "items" => "items#create"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
