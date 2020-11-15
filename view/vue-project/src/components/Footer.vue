d0bc9811420b19a76cf714da5ebe18d653440fdf
Kugue99A <hisabisa99yohey@gmail.com>
2020-11-15 09:23:37 +0900

    [mod]mypage

--- view/vue-project/src/components/Footer.vue
+++ view/vue-project/src/components/Footer.vue
@@ -1,2 +1,100 @@
 <template>
+  <v-footer
+    color="white"
+		app
+    padless
+  >
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="text-center"
+      >
+		<v-divider></v-divider>
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        color="#E91E63"
+        text
+        rounded
+        class="my-2"
+				@click.stop="dialog1 = true"
+      >
+			Display
+      </v-btn>
+			<v-btn
+				color="#E91E63"
+				text
+				class="my-2"
+				@click.stop="dialog2 = true"
+			>
+			Analysis
+			</v-btn>
+      <v-btn
+       v-show="!hidden"
+       color="pink"
+       dark
+       absolute
+       top
+       right
+       fab
+       >
+        <v-icon>mdi-plus</v-icon>
+    </v-btn>
+
+    <v-dialog
+      v-model="dialog1"
+      max-width="600"
+			hide-overlay
+    >
+      <v-card
+			outlined
+			class="mx-auto"
+			height="600"
+			>
+        <v-card-title class="headline">
+          Use Google's location service?
+        </v-card-title>
+
+        <v-card-text>
+          Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+        </v-card-text>
+
+      </v-card>
+    </v-dialog>
+     <v-dialog
+      v-model="dialog2"
+      max-width="600"
+			hide-overlay
+    >
+      <v-card
+			outlined
+			class="mx-auto"
+			height="600"
+			>
+        <v-card-title class="headline">
+          Use Google's location service?
+        </v-card-title>
+
+        <v-card-text>
+          Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+        </v-card-text>
+
+      </v-card>
+    </v-dialog>
+    </v-row>
+		</v-card>
+  </v-footer>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        dialog1: false,
+				dialog2: false,
+      }
+    },
+  }
+</script>
--- view/vue-project/src/components/Header.vue
+++ view/vue-project/src/components/Header.vue
@@ -1,2 +1,67 @@
 <template>
+  <div>
+    <v-app-bar app dark dense color="#E91E63">
+      <v-row
+        align="center"
+      >
+        <v-btn
+          dark
+					text
+          @click.stop="drawer = !drawer"
+        >
+        <v-icon dark>mdi-format-list-bulleted</v-icon>
+        </v-btn>
+      <v-col cols="6">
+        <v-toolbar-title>Discloth</v-toolbar-title>
+      </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'About', icon: 'mdi-forum' },
+        ],
+      }
+    },
+  }
+</script>
